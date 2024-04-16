af7708aee89ce2af76b4960419f38e0b7b0dd5cd
geromequa <gerome.quantmeye@hotmail.de>
2024-04-16 20:13:07 +0200

    worked on parpuz

---
+++ source/parpuz.cpp
@@ -0,0 +1,76 @@
+
+#include <bits/stdc++.h>
+
+#define rep(a, b) for (int a = 0; a < (b); ++a)
+#define all(a) (a).begin(), (a).end()
+#define endl '\n'
+
+using namespace std;
+using Graph = vector<vector<int>>;
+using ll = long long;
+
+int checkStringBalance(vector<pair<int, int>> &asc, vector<pair<int, int>> &desc)
+{
+    sort(asc.begin(), asc.end());
+
+    sort(desc.begin(), desc.end());
+    int y = 0;
+    rep(i, asc.size())
+    {
+        if (y - asc[i].first < 0)
+            return 0;
+        y += asc[i].second;
+    }
+    rep(i, desc.size())
+    {
+        if (y - desc[i].first < 0)
+            return 0;
+        y += desc[i].second;
+    }
+    return y == 0;
+}
+pair<int, int> parseString(string &s)
+{
+    int lowest = 0;
+    int current = 0;
+    rep(c, s.size())
+    {
+        if (s[c] == ')')
+        {
+            current--;
+            lowest = min(lowest, current);
+        }
+        else if (s[c] == '(')
+        {
+            current++;
+        };
+    }
+    if (lowest > 0)
+        lowest = 0;
+    return make_pair(abs(lowest), current);
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.precision(10);
+
+    int t;
+    cin >> t;
+    string s;
+    vector<string> strings;
+    vector<pair<int, int>> asc;
+    vector<pair<int, int>> desc;
+
+    rep(i, t)
+    {
+        pair<int, int> current;
+        cin >> s;
+        strings.push_back(s);
+        current = parseString(s);
+        (current.second >= 0) ? asc.push_back(current) : desc.push_back(current);
+    }
+    cout << (checkStringBalance(asc, desc) ? "YES" : "NO") << endl;
+    return 0;
+}
