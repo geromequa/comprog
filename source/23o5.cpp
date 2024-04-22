de7f6148f2f61c46e9c1ccf140958dad8f2393cb
geromequa <gerome.quantmeye@hotmail.de>
2024-04-22 15:52:43 +0000

    started working on 23o5

---
+++ source/23o5.cpp
@@ -0,0 +1,65 @@
+
+#include <bits/stdc++.h>
+
+#define rep(a, b) for (int a = 0; a < (b); ++a)
+#define all(a) (a).begin(), (a).end()
+#define endl '\n'
+
+using namespace std;
+using Graph = vector<vector<int>>;
+using ll = long long;
+
+ll useOperator(int operatorID, ll a, ll b)
+{
+    switch (operatorID)
+    {
+    case 0:
+        return a + b;
+    case 1:
+        return a - b;
+    case 2:
+        return a * b;
+    }
+}
+
+string parseNumbers(vector<ll> numbers)
+{
+    while (next_permutation(all(numbers)))
+    {
+        rep(i, 2)
+        {
+            rep(j, 2)
+            {
+                rep(k, 2)
+                {
+                    rep(l, 2)
+                    {
+                        if (useOperator(l, useOperator(k, useOperator(j, useOperator(i, numbers[0], numbers[1]), numbers[2]), numbers[3]), numbers[4]) == 23)
+                            return "Possible";
+                    }
+                }
+            }
+        }
+    }
+    return "Impossible";
+}
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.precision(10);
+
+    int t;
+    cin >> t;
+    vector<ll> numbers(5);
+    rep(i, t)
+    {
+        for (auto &num : numbers)
+        {
+            cin >> num;
+        }
+        cout << parseNumbers(numbers) << endl;
+    }
+
+    return 0;
+}
