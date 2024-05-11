3b7221f052b8c4e30c28667d6535cfda4379b8eb
geromequa <gerome.quantmeyer@hotmail.com>
2024-05-11 14:23:37 +0000

    worked on ip-lookup

--- source/ip-lookup.cpp
+++ source/ip-lookup.cpp
@@ -9,20 +9,61 @@ using namespace std;
 using Graph = vector<vector<int>>;
 using ll = long long;
 
-int main()
-{
+struct TrieNode {
+    bool isEndOfWord;
+    unordered_map<char, TrieNode*> children;
+};
+
+TrieNode* getNode() {
+    TrieNode* node = new TrieNode;
+    node->isEndOfWord = false;
+    return node;
+}
+
+void insert(TrieNode* root, string key) {
+    TrieNode* current = root;
+    for (int i = 0; i < key.length(); i++) {
+        char index = key[i];
+        if (!current->children[index])
+            current->children[index] = getNode();
+        current = current->children[index];
+        if (key[i+1] == '*')
+            current->isEndOfWord = true;
+    }
+    current->isEndOfWord = true;
+}
+
+bool search(TrieNode* root, string key) {
+    TrieNode* current = root;
+    for (int i = 0; i < key.length(); i++) {
+        char index = key[i];
+        if (current->children['*'])
+            return true;
+        if (!current->children[index])
+            return false;
+        current = current->children[index];
+    }
+    return (current != NULL && current->isEndOfWord);
+}
+
+int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.precision(10);
 
-    ll t;
-    cin >> t;
-    vector<ll> input(t);
-    for (auto &i : input)
-    {
-        cin >> i;
+    int n, m;
+    cin >> n;
+    TrieNode* root = getNode();
+    rep (i,n) {
+        string ip;
+        cin >> ip;
+        insert(root, ip);
     }
-    // content
-
+    cin >> m;
+    rep(i,m) {
+        string ip;
+        cin >> ip;
+        cout << (search(root, ip) ? "Yes" : "No") << '\n';
+    }
     return 0;
 }
