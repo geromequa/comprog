fdab1518473ec28757b250c82b127ca70da10216
geromequa <gerome.quantmeyer@hotmail.com>
2024-05-15 14:44:32 +0000

    created framework for vladimir

--- source/vladimir.cpp
+++ source/vladimir.cpp
@@ -9,37 +9,25 @@ using namespace std;
 using Graph = vector<vector<int>>;
 using ll = long long;
 
-int main() {
+vector<ll> vladimir(vector<ll> a)
+{
+    return a;
+}
+
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.precision(10);
 
-    int k, c;
+    ll k, c;
     cin >> k >> c;
-    vector<int> colors(k);
-    for (int i = 0; i < k; i++) {
-        cin >> colors[i];
-    }
-
-    int left = 0, right = k - 1, counter = 0;
-    vector<int> possible_n;
-    while (left <= right) {
-        if (colors[left] == colors[right]) {
-            counter++;
-            left++;
-            right--;
-        } else {
-            possible_n.push_back(right + 1 + counter);
-            counter = 0;
-            right--;
-        }
-    }
-    possible_n.push_back(right + 1 + counter);
-
-    for (int n : possible_n) {
-        cout << n << ' ';
-    }
-    cout << '\n';
-
+    vector<ll> a(k);
+    for (ll &x : a)
+        cin >> x;
+    vector<ll> res = vladimir(a);
+    for (ll x : res)
+        cout << x << ' ';
+    cout << k << endl;
     return 0;
 }
