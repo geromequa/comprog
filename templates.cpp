bd2542011e5bd6e6e1cdc22f44503558c0a729c9
geromequa <gerome.quantmeyer@hotmail.com>
2024-06-21 21:17:07 +0000

    added templates

--- templates.cpp
+++ templates.cpp
@@ -154,3 +154,50 @@ pair<vector<ll>, vector<ll>> dijkstra(const weighted_graph &g, ll start)
 
     return make_pair(dist, prev);
 }
+
+// Union find data structure
+struct DisjointSet
+{
+    vector<int> parent;
+    vector<int> size;
+
+    DisjointSet(int maxSize)
+    {
+        parent.resize(maxSize);
+        size.resize(maxSize);
+        rep(i, maxSize)
+        {
+            parent[i] = i;
+            size[i] = 1;
+        }
+    }
+
+    int find_set(int v)
+    {
+        if (v == parent[v])
+        {
+            return v;
+        }
+        return parent[v] = find_set(parent[v]);
+    }
+
+    void union_set(int a, int b)
+    {
+        a = find_set(a);
+        b = find_set(b);
+        if (a != b)
+        {
+            if (size[a] < size[b])
+            {
+                swap(a, b);
+            }
+            parent[b] = a;
+            size[a] += size[b];
+        }
+    }
+
+    int get_size(int v)
+    {
+        return size[v];
+    }
+};
