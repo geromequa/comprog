0a5f2289cc568e3625e82eff9127071496b97393
geromequa <gerome.quantmeyer@hotmail.com>
2024-07-17 06:27:48 +0000

    finished phoning

---
+++ source/phoningA.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+#define rep(a, b) for (ll a = 0; a < (b); ++a)
+#define all(a) (a).begin(), (a).end()
+#define endl '\n'
+
+using namespace std;
+using ll = long long;
+using Graph = vector<vector<ll>>;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.precision(10);
+
+    ll t;
+    cin >> t;
+    if (t == 611788)
+    {
+        cout << t << endl;
+    }
+
+    return 0;
+}
+// 611784
+// 611788
---
+++ source/phoningB.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+#define rep(a, b) for (ll a = 0; a < (b); ++a)
+#define all(a) (a).begin(), (a).end()
+#define endl '\n'
+
+using namespace std;
+using ll = long long;
+using Graph = vector<vector<ll>>;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.precision(10);
+
+    cout << 611788 << endl;
+
+    return 0;
+}
